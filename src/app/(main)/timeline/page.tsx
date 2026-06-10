7e6650df932a4a753881f387eee5dcdcdfdf51d3
Quan Nguyen <quannnguyen2009@gmail.com>
2026-06-10 11:10:21 +0700

    fix again

--- src/app/(main)/timeline/page.tsx
+++ src/app/(main)/timeline/page.tsx
@@ -1,7 +1,7 @@
 export default function TimelinePage() {
   return (
     <div className="bg-white h-screen w-screen flex items-center justify-center">
-        <iframe src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=v2%3A2PACX-1vT1TFYV_fvnS_O6uHX4-l6aV_xnc3ncj4Z-J7vcuv8ntRNwvcoq05M7j4vfSqwq4V2OzUl1V4q4rpd0&font=Default&lang=en&initial_zoom=2&width=100%25&height=650' width='100%' height='650' allowfullscreen frameborder='0'></iframe>
+      <iframe src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=v2%3A2PACX-1vT1TFYV_fvnS_O6uHX4-l6aV_xnc3ncj4Z-J7vcuv8ntRNwvcoq05M7j4vfSqwq4V2OzUl1V4q4rpd0&font=Default&lang=en&initial_zoom=2&width=100%25&height=100%25' width='100%' height='100%' webkitallowfullscreen mozallowfullscreen allowfullscreen frameborder='0'></iframe>
     </div>
   )
 }
