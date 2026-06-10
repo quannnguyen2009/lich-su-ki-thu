39d905251fab84f144801b1d366b1309a17106b0
Quan Nguyen <quannnguyen2009@gmail.com>
2026-06-10 10:40:35 +0700

    fix timeline syntax

--- src/app/(main)/timeline/page.tsx
+++ src/app/(main)/timeline/page.tsx
@@ -4,3 +4,4 @@ export default function TimelinePage() {
         <iframe src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=v2%3A2PACX-1vT1TFYV_fvnS_O6uHX4-l6aV_xnc3ncj4Z-J7vcuv8ntRNwvcoq05M7j4vfSqwq4V2OzUl1V4q4rpd0&font=Default&lang=en&initial_zoom=2&width=100%25&height=650' width='100%' height='650' allowfullscreen frameborder='0'></iframe>
     </div>
   )
+}
